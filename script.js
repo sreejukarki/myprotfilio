56012e9d55ee72c4ebbf732cd5cf1dad8e54edda
sreejukarki <119788966+sreejukarki@users.noreply.github.com>
2023-04-18 09:18:53 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("i m running")
+document.querySelector(".cross").style.display = 'none';
+document.querySelector(".hamburger").addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo')
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector(".ham").style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+
+    }
+    else
+    {
+        setTimeout(()=> {
+        document.querySelector(".cross").style.display = 'inline'
+    }, 350);
+        document.querySelector('.ham').style.display = 'none'
+        
+    }
+})
